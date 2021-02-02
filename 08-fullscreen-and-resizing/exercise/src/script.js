e1585d0336e8a190a0168ec9cd9a360d62710e7b
FrazierMark <60164353+FrazierMark@users.noreply.github.com>
2021-02-02 07:49:46 -0600

    learning fullscreen and resizing with three.js continued.

--- 08-fullscreen-and-resizing/exercise/src/script.js
+++ 08-fullscreen-and-resizing/exercise/src/script.js
@@ -23,9 +23,36 @@ scene.add(mesh)
  * Sizes
  */
 const sizes = {
-    width: 800,
-    height: 600
+    width: window.innerWidth,
+    height: window.innerHeight
 }
+
+window.addEventListener('resize', () => 
+{
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    //Update Camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    //Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+window.addEventListener('dblclick', () =>
+{
+    if(!document.fullscreenElement)
+    {
+        canvas.requestFullscreen()
+    }
+    else
+    {
+        document.exitFullscreen()
+    }
+
+})
 
 /**
  * Camera
