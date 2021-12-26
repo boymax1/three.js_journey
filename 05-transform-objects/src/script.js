330e9333ee30e63a48dc0fc8aa696dfc16a30e29
FrazierMark <m.mark.frazier@gmail.com>
2021-12-26 09:32:17 -0800

    Finished Lesson on Transform

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
+import { meshBounds } from '@react-three/drei'
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -19,7 +20,37 @@ box.position.z = 1
 
 scene.add(box)
 
+// add cubes to group and now we can adjust position, scale rotation to all objects added to group
+const group = new THREE.Group()
+scene.add(group)
+
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+cube2.position.x = -2
+group.add(cube2);
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+cube3.position.x = 2
+group.add(cube3);
+
 console.log(box.position.length()); // distance from center of center to our object
+
+// //scale
+box.scale.x = 2;
+box.scale.y = 2; 
+
+box.rotation.y = Math.PI;
 
 // reduces vector to 1
 // box.position.normalize();
@@ -45,6 +76,8 @@ const sizes = {
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
+
+//camera.lookAt(box.position);
 
 // distance from camera to box
 console.log(box.position.distanceTo(camera.position));
