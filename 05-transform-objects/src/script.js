cc2c9c08f8f133722b1b81db8ff9bd28179bf2a3
FrazierMark <m.mark.frazier@gmail.com>
2021-12-26 09:16:15 -0800

    create box mesh, camera, and position of camera

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -12,8 +12,24 @@ const scene = new THREE.Scene()
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const box = new THREE.Mesh(geometry, material)
+box.position.x = 0.7
+box.position.y = -0.6
+box.position.z = 1
+
+scene.add(box)
+
+console.log(box.position.length()); // distance from center of center to our object
+
+// reduces vector to 1
+// box.position.normalize();
+
+// use set() to change position all at same time
+// box.position.set(1, 2, 1)
+
+//axis helper
+const axisHelper = new THREE.AxesHelper(2);
+scene.add(axisHelper);
 
 /**
  * Sizes
@@ -29,6 +45,9 @@ const sizes = {
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
+
+// distance from camera to box
+console.log(box.position.distanceTo(camera.position));
 
 /**
  * Renderer
