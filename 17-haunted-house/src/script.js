821851a81707bca4580efb5ac26df69a74395a96
FrazierMark <m.mark.frazier@gmail.com>
2022-03-08 16:39:14 -0800

    Finish lesson 17 haunted house

--- 17-haunted-house/src/script.js
+++ 17-haunted-house/src/script.js
@@ -182,9 +182,35 @@ for(let i = 0; i < 50; i++)
     grave.rotation.z = (Math.random() - 0.5) * 0.4
     grave.rotation.y = (Math.random() - 0.5) * 0.4
 
+    grave.castShadow = true
+
     // Add to the graves container
     graves.add(grave)
 }
+floor.receiveShadow = true
+
+//Ghosts (floating lights)
+const ghost1 = new THREE.PointLight('#ff00ff', 2, 3)
+scene.add(ghost1)
+
+ghost1.shadow.mapSize.width = 256
+ghost1.shadow.mapSize.height = 256
+ghost1.shadow.camera.far = 7
+
+const ghost2 = new THREE.PointLight('#00ffff', 2, 3)
+scene.add(ghost2)
+
+ghost2.shadow.mapSize.width = 256
+ghost2.shadow.mapSize.height = 256
+ghost2.shadow.camera.far = 7
+
+const ghost3 = new THREE.PointLight('#ffff00', 2, 3)
+scene.add(ghost3)
+
+ghost3.shadow.mapSize.width = 256
+ghost3.shadow.mapSize.height = 256
+ghost3.shadow.camera.far = 7
+
 
 
 /**
@@ -202,18 +228,43 @@ gui.add(moonLight, 'intensity').min(0).max(1).step(0.001)
 gui.add(moonLight.position, 'x').min(- 5).max(5).step(0.001)
 gui.add(moonLight.position, 'y').min(- 5).max(5).step(0.001)
 gui.add(moonLight.position, 'z').min(- 5).max(5).step(0.001)
+
+moonLight.shadow.mapSize.width = 256
+moonLight.shadow.mapSize.height = 256
+moonLight.shadow.camera.far = 15
+
 scene.add(moonLight)
 
 
 // Door Light
 const doorLight = new THREE.PointLight('#ff7d46', 1, 7) // color, intensity and distance
 doorLight.position.set(0, 2.2, 2.7)
+
+moonLight.castShadow = true
+doorLight.castShadow = true
+ghost1.castShadow = true
+ghost2.castShadow = true
+ghost3.castShadow = true
+
+doorLight.shadow.mapSize.width = 256
+doorLight.shadow.mapSize.height = 256
+doorLight.shadow.camera.far = 7
+
 house.add(doorLight)
 
 
 // Fog
 const fog = new THREE.Fog('#262837', 1, 15) // near and far arguments
 scene.fog = fog
+
+
+
+walls.castShadow = true
+bush1.castShadow = true
+bush2.castShadow = true
+bush3.castShadow = true
+bush4.castShadow = true
+
 
 
 /**
@@ -263,6 +314,11 @@ renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 renderer.setClearColor('#262837')
 
+renderer.shadowMap.enabled = true;
+
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+
+
 
 /**
  * Animate
@@ -272,6 +328,22 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+        // Ghosts
+        const ghost1Angle = elapsedTime * 0.5
+        ghost1.position.x = Math.cos(ghost1Angle) * 4
+        ghost1.position.z = Math.sin(ghost1Angle) * 4
+        ghost1.position.y = Math.sin(elapsedTime * 3)
+    
+        const ghost2Angle = - elapsedTime * 0.32
+        ghost2.position.x = Math.cos(ghost2Angle) * 5
+        ghost2.position.z = Math.sin(ghost2Angle) * 5
+        ghost2.position.y = Math.sin(elapsedTime * 4) + Math.sin(elapsedTime * 2.5)
+    
+        const ghost3Angle = - elapsedTime * 0.18
+        ghost3.position.x = Math.cos(ghost3Angle) * (7 + Math.sin(elapsedTime * 0.32))
+        ghost3.position.z = Math.sin(ghost3Angle) * (7 + Math.sin(elapsedTime * 0.5))
+        ghost3.position.y = Math.sin(elapsedTime * 4) + Math.sin(elapsedTime * 2.5)
 
     // Update controls
     controls.update()
